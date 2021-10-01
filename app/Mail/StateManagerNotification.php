844205d559bd113d50c8be2e300602f177773054
rafaelecs1 <74567820+rafaelecs1@users.noreply.github.com>
2021-09-30 22:27:33 -0300

    enviando email para adesoes estaduais

---
+++ app/Mail/StateManagerNotification.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace BuscaAtivaEscolar\Mail;
+
+
+use BuscaAtivaEscolar\StateSignup;
+use Illuminate\Mail\Mailable;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class StateManagerNotification extends Mailable
+{
+
+  protected $state_signup;
+
+  public function __construct(StateSignup $state_signup)
+  {
+    $this->state_signup = $state_signup;
+  }
+
+  /**
+   * Build the message.
+   *
+   * @return $this
+   */
+  public function build()
+  {
+    $message = (new MailMessage())
+      ->success()
+      ->subject("[Busca Ativa Escolar] Adesão estadual")
+      ->line("Sr(a). Gestor(a) Estadual " . $this->state_signup->data["admin"]["name"])
+      ->line("Seu pedido de adesão/readesão à estratégia Busca Ativa Escolar foi aprovado. Por favor, confirme a adesão/readesão no botão abaixo.")
+      ->action('Confirmar adesão/readesão', $this->getUrlConfirmSignup());
+
+    $this->subject("[Busca Ativa Escolar] Adesão estadual");
+
+    $this->withSwiftMessage(function ($message) {
+      $headers = $message->getHeaders();
+      $headers->addTextHeader('message-id', $this->state_signup->id);
+    });
+
+    return $this->view('vendor.notifications.email', $message->toArray());
+  }
+
+  protected function getUrlConfirmSignup()
+  {
+    return env('APP_PANEL_URL') . "/confirmacao_gestor_estadual/" . $this->state_signup->id;
+  }
+}
