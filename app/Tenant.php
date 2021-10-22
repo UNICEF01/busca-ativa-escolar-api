5d136a60b9bb603e370ab111f4081b91d2ef78a4
Manoel Souza <mansouza@unicef.org>
2021-10-22 00:01:24 -0300

    Setando lgpd = 1 para gesores politicos no ato do cadastro

--- app/Tenant.php
+++ app/Tenant.php
@@ -340,7 +340,7 @@ class Tenant extends Model  {
 			throw new ValidationException('invalid_operational_admin_data', $validator);
 		}
 
-        $politicalAdmin->lgpd = 0;
+        $politicalAdmin->lgpd = 1;
         $operationalAdmin->lgpd = 0;
 
 		$politicalAdmin->save();
@@ -423,7 +423,7 @@ class Tenant extends Model  {
             throw new ValidationException('invalid_political_admin_data', $validator);
         }
 
-        $politicalAdmin->lgpd = 0;
+        $politicalAdmin->lgpd = 1;
 
         //cria a estrutura do novo coordenador se existir
         if(sizeof($operationalAdminData) > 0){
