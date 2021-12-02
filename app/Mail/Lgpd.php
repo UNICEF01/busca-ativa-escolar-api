c3832671b35fea155291763b23e8247afffa6c03
Rafael Emerick <rcorreia@unicef.org>
2021-12-02 14:38:29 +0000

    corrigindo a rota dos arquivos

--- app/Mail/Lgpd.php
+++ app/Mail/Lgpd.php
@@ -23,9 +23,9 @@ class Lgpd extends Mailable
       ->subject("Busca Ativa Escolar - Relatório LGPD")
       ->line("Prezado(a),")
       ->line("Este é o relatório do aceite do termo dos municípios, estados e dos usuários exportado hoje às 9h.");
-    $this->attach('/home/rsantos/rel/relatorio_02_12_2021_estadual.csv');
-    $this->attach('/home/rsantos/rel/relatorio_02_12_2021_municipal.csv');
-    $this->attach('/home/rsantos/rel/relatorio_02_12_2021_usuario.csv');
+    $this->attach('/home/forge/rel/relatorio_02_12_2021_estadual.csv');
+    $this->attach('/home/forge/rel/relatorio_02_12_2021_municipal.csv');
+    $this->attach('/home/forge/rel/relatorio_02_12_2021_usuario.csv');
     $this->subject("Busca Ativa Escolar - Relatório LGPD");
 
     $this->withSwiftMessage(function ($message) {
