36486d6c055c2e0963df535fb7de614987afe455
Manoel Souza <mansouza@unicef.org>
2022-06-30 10:35:54 -0300

    Alterando o arquivo ChildActivityLogger para escutar modificacoes nas entidades Child e assim mudar o campo tree_id nos cases relacionados

--- app/Observers/ChildActivityLogObserver.php
+++ app/Observers/ChildActivityLogObserver.php
@@ -22,10 +22,14 @@ class ChildActivityLogObserver {
 	public function created(Child $child) {
 		ActivityLog::writeEntry($child, 'created', ['child_name' => $child->name, 'child' => $child, 'request' => request()->all()], ['source' => get_class()]);
 	}
-//Todo vericar a necessidade desse update
-//	public function updated(Child $child) {
-//		ActivityLog::writeEntry($child, 'updated', ['child_name' => $child->name, 'child' => $child, 'request' => request()->all()], ['source' => get_class()]);
-//	}
+	public function updated(Child $child) {
+        $case = $child->cases()->get()[0];
+		$group = $case->group;
+        $ids = $group->getArrayOfParentsId();
+        array_push($ids, $group->id);
+        $case->tree_id = implode(", ", $ids);
+        $case->save();
+	}
 
 	public function deleted(Child $child) {
 		ActivityLog::writeEntry($child, 'deleted', ['child_name' => $child->name, 'child' => $child, 'request' => request()->all()], ['source' => get_class()]);
