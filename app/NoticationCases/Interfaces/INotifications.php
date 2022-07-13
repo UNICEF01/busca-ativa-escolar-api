e6e5f4851fbc58e45bb26426fb1aa2b8a1a5c49f
Rafael Emerick <rcorreia@unicef.org>
2022-07-13 20:31:27 -0300

    adicionando as notificacoes

---
+++ app/NoticationCases/Interfaces/INotifications.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BuscaAtivaEscolar\NotificationsCases\Interfaces;
+
+interface INotifications
+{
+  public function saveNotificationData(array $attributes): object;
+  public function findNotificationData(string $id): ?object;
+  public function findAllNotificationDataByUser(string $treeId): ?object;
+  public function deleteNotificationData(string $id): bool;
+  public function resolveNotificationData(string $id): bool;
+  public function getTrees(string $id): string;
+}
---
+++ app/NoticationCases/Repositories/NotificationCasesRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BuscaAtivaEscolar\NotificationsCases\Repositories;
+
+
+use BuscaAtivaEscolar\NotificationCases;
+use BuscaAtivaEscolar\LGPD\Repository\BaseRepository;
+use Illuminate\Database\Eloquent\Model;
+
+
+class NotificationCasesRepository extends BaseRepository
+{
+  protected $notificationCases;
+
+  public function __construct(NotificationCases $notificationCases)
+  {
+    parent::__construct($notificationCases);
+  }
+
+  public function save(array $attributes): Model
+  {  
+    $dataNotification = new $this->model;
+    $dataNotification->tenant_id = $attributes['tenant_id'];
+    $dataNotification->user_id = $attributes['user_id'];
+    $dataNotification->children_case_id = $attributes['children_case_id'];
+    $dataNotification->notification = $attributes['notification'];
+    $dataNotification->case_tree_id = $attributes['case_tree_id'];
+    $dataNotification->users_tree_id = $attributes['users_tree_id'];
+    $dataNotification->solved = 0;
+    $dataNotification->save();
+    return $dataNotification->fresh();
+  }
+
+  public function find(string $id): ?Model
+  {
+    return $this->model->where('id', $id)->get();
+  }
+
+  public function delete(string $id): bool
+  {
+    return $this->model->find($id)->delete();
+  }
+
+  public function findAll(string $tree_id): ?Model
+  {
+    return $this->model->where('users_tree_id', $tree_id)->get();
+  }
+
+  public function update(array $attributes, string $id): bool
+  {
+    return $this->model->find($id)->update('solved', 1);
+  }
+}
