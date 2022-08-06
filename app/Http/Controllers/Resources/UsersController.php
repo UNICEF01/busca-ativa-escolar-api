ec59cdb8b2e7da8e18e8641149becf7b73c5c55c
Rafael Emerick <rcorreia@unicef.org>
2022-08-06 15:30:44 -0300

    corrigindo funcao de update

--- app/Http/Controllers/Resources/UsersController.php
+++ app/Http/Controllers/Resources/UsersController.php
@@ -256,7 +256,7 @@ class UsersController extends BaseController
                 if(!strpos($user->tree_id, $input['group_id'])){
                     $client = \Elasticsearch\ClientBuilder::create()->setHosts(['localhost:9200'])->build();
                     $updateRequest = ['index' => 'children', 'body' => ['query' => ['bool' => ['filter' => ['terms' => ['_id' => [],],],],], 'script' => ['inline' => "ctx._source.assigned_user_id = null; ctx._source.assigned_user_name = null; ctx._source.assigned_group_name = null"]]];
-                    $updateRequest2 = ['index' => 'children', 'body' => ['query' => ['bool' => ['filter' => ['terms' => ['_id' => [],],],],], 'script' => ['inline' => "ctx._source.assigned_user_id = {$input['id']}; ctx._source.assigned_user_name = {$input['name']}; ctx._source.assigned_group_name = {$input['group_name']}; ctx._source.assigned_group_id = {$input['group_id']}"]]];
+                    $updateRequest2 = ['index' => 'children', 'body' => ['query' => ['bool' => ['filter' => ['terms' => ['_id' => [],],],],], 'script' => ['inline' => "ctx._source.assigned_user_id = '{$input['id']}'; ctx._source.assigned_user_name = '{$input['name']}'; ctx._source.assigned_group_name = '{$input['group_name']}'; ctx._source.assigned_group_id = '{$input['group_id']}'"]]];
                     $groups = implode(', ', Group::where('id', $input['group_id'])->get()->first()->getTree());
                     foreach (ChildCase::whereHas('currentStep', function (Builder $query) use ($user) {
                         $query->where('assigned_user_id', '=', $user->id);
