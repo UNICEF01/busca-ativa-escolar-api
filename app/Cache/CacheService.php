6e04d2de04b0bed6395ae468ecac6f8a1afbdb83
rafaelecs1 <74567820+rafaelecs1@users.noreply.github.com>
2022-12-20 09:12:02 -0300

    mudando o serviço de cache para os panéis.

--- app/Cache/CacheService.php
+++ app/Cache/CacheService.php
@@ -34,64 +34,57 @@ class CacheService extends BaseController
     public function returnData(string $searched, bool $check = false): array
     {
         $reg = ['N', 'NE', 'CO', 'SD', 'S', 'BR'];
-
-        $cache = Cache::get("report_cache");
-        $data = explode("\n", $cache);
-        $states = explode(" ", $data[0]);
-        $tenants = explode(" ", $data[1]);
-        $tenantsSignup = explode(" ", $data[2]);
-        $alerts = explode(" ", $data[3]);
-        $cases = explode(" ", $data[4]);
-        $rcases = explode(" ", $data[5]);
-
-        $stateIndex = $this->search($states, count($states), $searched);
-        $tenantsIndex = $this->search($tenants, count($tenants), $searched);
-        $tenantsSignupIndex = $this->search($tenantsSignup, count($tenantsSignup), $searched);
-        $alertsIndex = $this->search($alerts, count($alerts), $searched);
-        $casesIndex = $this->search($cases, count($cases), $searched);
-        $rcasesIndex = $this->search($rcases, count($rcases), $searched);
-        $rcasesIndex += 1;
-        $causes = [];
+        $searched = $searched == 'BR' ? '' : $searched;
+        $reason_causes = [];
         if (!in_array($searched, $reg)) {
             $tenantIDs = Tenant::getIDsWithinUF($searched);
             $cityIDs = City::getIDsWithinUF($searched);
         }
+        $data = [];
+        $ufs = Cache::get("state_signup_" . $searched);
+        $tenants_signup = explode(" ", Cache::get("tenant_signup_" . $searched));
+        $tenants = explode(" ", Cache::get("tenants_" . $searched));
+        $alerts = explode(" ", Cache::get("alerts_" . $searched));
+        $causes = explode(" ", Cache::get("cases_" . $searched));
+        $reasons = explode(" ", Cache::get("reason_cases_" . $searched));
+        $i = 0;
+        
         foreach (CaseCause::getAll() as $case) {
-
-            array_push($causes, ['id' => $case->id, 'cause' => $case->label, 'qtd' => $rcases[$rcasesIndex++]]);
+            array_push($reason_causes, ['id' => $case->id, 'cause' => $case->label, 'qtd' => $reasons[$i++]]);
         }
-        $data = [
-            'ufs' => [
-                'is_approved' => $states[$stateIndex + 1],
-                'num_ufs' => $states[$stateIndex + 1],
-                'num_pending_state_signups' =>  in_array($searched, $reg) ? $states[$stateIndex + 2] : 0,
+        
+        $data  = [
+            "ufs" => [
+                "is_approved" => "$ufs",
+                "num_ufs" => "$ufs",
+                "num_pending_state_signups" => "0",
             ],
-            'tenants' => [
-                'num_tenants' => $tenants[$tenantsIndex + 1],
-                'active' => $tenants[$tenantsIndex + 2],
-                'inactive' => $tenants[$tenantsIndex + 3],
-                'num_signups' => $tenantsSignup[$tenantsSignupIndex + 1],
-                'num_pending_setup' => $tenantsSignup[$tenantsSignupIndex + 2],
-                'num_pending_signups' => $tenantsSignup[$tenantsSignupIndex + 3],
+            "tenants" => [
+                "num_tenants" => "$tenants[0]",
+                "active" => "$tenants[1]",
+                "inactive" => "$tenants[2]",
+                "num_signups" => '' . ($tenants[0] + $tenants_signup[1]) . '',
+                "num_pending_setup" => "$tenants_signup[1]",
+                "num_pending_signups" => "$tenants_signup[2]"
             ],
-            'alerts' => [
-                '_total' => intval($alerts[$alertsIndex + 1]) + intval($alerts[$alertsIndex + 2]) + intval($alerts[$alertsIndex + 3]),
-                '_approved' => $alerts[$alertsIndex + 1],
-                '_pending' => $alerts[$alertsIndex + 2],
-                '_rejected' => $alerts[$alertsIndex + 3],
+            "alerts" => [
+                "_total" => '' . $alerts[0] + $alerts[1] + $alerts[2] . '',
+                "_approved" => "$alerts[0]",
+                "_pending" => "$alerts[1]",
+                "_rejected" => "$alerts[2]"
             ],
-            'cases' => [
-                '_total' => $cases[$casesIndex + 3] + $cases[$casesIndex + 4] + $cases[$casesIndex + 5] + $cases[$casesIndex + 6] + $cases[$casesIndex + 7] + $cases[$casesIndex + 8],
-                '_in_progress' => $cases[$casesIndex + 1],
-                '_enrollment' => $cases[$casesIndex + 2],
-                '_in_school' => $cases[$casesIndex + 3],
-                '_in_observation' => $cases[$casesIndex + 4],
-                '_out_of_school' => $cases[$casesIndex + 5],
-                '_cancelled' => $cases[$casesIndex + 6],
-                '_transferred' => $cases[$casesIndex + 7],
-                '_interrupted' => $cases[$casesIndex + 8],
+            "cases" => [
+                '_total' => '' . $causes[2] + $causes[3] + $causes[4] + $causes[5] + $causes[6] + $causes[7] . '',
+                '_in_progress' => "$causes[0]",
+                '_enrollment' => "$causes[1]",
+                '_in_school' => "$causes[2]",
+                '_in_observation' => "$causes[3]",
+                '_out_of_school' => "$causes[4]",
+                '_cancelled' => "$causes[5]",
+                '_transferred' => "$causes[6]",
+                '_interrupted' => "$causes[7]",
             ],
-            'causes_cases' => $causes,
+            'causes_cases' => $reason_causes,
             'tenant_ids' => $check ? $tenantIDs : '',
             'city_ids' => $check ? $cityIDs : ''
         ];
