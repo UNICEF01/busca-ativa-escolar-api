79a1e3980b01f9253166e9fe9583a6ab7e3e7ed5
manoelfilho <manoel.tsf@gmail.com>
2022-12-28 00:18:19 -0300

    bloqueio teporario de autenticacoes

--- app/Http/Controllers/Auth/IdentityController.php
+++ app/Http/Controllers/Auth/IdentityController.php
@@ -38,6 +38,8 @@ class IdentityController extends BaseController
     public function authenticate(Request $request)
     {
 
+        return response()->json(['error' => 'invalid_credentials'], 401);
+
         if (request('grant_type', 'login') == "refresh") {
             return $this->refresh($request);
         }
