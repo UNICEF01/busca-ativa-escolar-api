a8bbc4d4a033208e3b73d7cf626faa4bacce57d4
manoelfilho <manoel.tsf@gmail.com>
2023-01-03 00:33:05 -0300

    Script para remocao de inconsistencias

--- app/Console/Commands/CheckInconsistenciesCases.php
+++ app/Console/Commands/CheckInconsistenciesCases.php
@@ -4,6 +4,7 @@ namespace BuscaAtivaEscolar\Console\Commands;
 
 use Illuminate\Console\Command;
 use DB;
+use Log;
 
 class CheckInconsistenciesCases extends Command
 {
@@ -19,7 +20,7 @@ class CheckInconsistenciesCases extends Command
      *
      * @var string
      */
-    protected $description = 'Checa casos de inconsistencias da base de dados e salva em arquivo csv';
+    protected $description = 'Checa casos de inconsistencias da base de dados, salva em arquivo csv e permite a remocao de todas as inconsistencias encontradas';
 
     /**
      * Create a new command instance.
@@ -39,15 +40,7 @@ class CheckInconsistenciesCases extends Command
     public function handle()
     {
 
-        //Consulta casos com alert_status diferentes nas tabelas Children e Alerta
-        $sqlCasosComAlertStatusDiferentes = "SELECT c.id, c.created_at, c.deleted_at, c.updated_at, c.educacenso_id, c.alert_submitter_id, c.tenant_id, c.alert_status as child_alert_status, csa.alert_status as alert_alert_status, c.name
-                    FROM children c
-                    JOIN case_steps_alerta csa ON csa.child_id = c.id
-                    where c.alert_status <> csa.alert_status";
-
-        $casosComAlertStatusDiferentes = $this->queryObject($sqlCasosComAlertStatusDiferentes);
-
-        //Consulta crianças sem valor no conjunto de tabelas relacionadas
+        //Consulta crianca com conjunto de tabelas do caso incompletas
         $sqlEstruturaDeCasoIncompleta = "SELECT c.id, c.created_at, c.deleted_at, c.updated_at, c.educacenso_id, c.alert_submitter_id, c.tenant_id, c.alert_status as child_alert_status, c.name 
                 FROM children c WHERE 
                 c.id NOT IN (SELECT child_id FROM case_steps_alerta) || 
@@ -60,8 +53,7 @@ class CheckInconsistenciesCases extends Command
 
         $casosEstruturaDeCasoIncompleta = $this->queryObject($sqlEstruturaDeCasoIncompleta);
 
-
-        //Consulta de criancas sem informacoes da etapa corrente - Possui step porem nao tem informacoes necessárias
+        //Consulta de criancas sem informacoes da etapa corrente - Possui step porem nao tem informacoes necessárias em children_case
         $slqCasosCriancasSemInformacaodeStep = "SELECT c.id, c.created_at, c.deleted_at, c.updated_at, c.educacenso_id, c.alert_submitter_id, c.tenant_id, c.alert_status as child_alert_status, c.name, c.current_case_id, c.current_step_id, c.current_step_type
                 FROM children c 
                 join case_steps_alerta csa ON csa.child_id = c.id
@@ -70,8 +62,16 @@ class CheckInconsistenciesCases extends Command
 
         $casosCriancasSemInformacaodeStep = $this->queryObject($slqCasosCriancasSemInformacaodeStep);
 
+        //criancas com nomes não registrados
+        $sqlAlertasSemNome = "SELECT c.tenant_id, c.id, c.created_at, c.name, c.mother_name, csa.alert_status, c.alert_status, c.child_status, cc.case_status FROM children c
+        join case_steps_alerta csa ON csa.child_id = c.id
+        join children_cases cc ON cc.child_id = c.id
+        where c.name = '-- informação não disponível --' and cc.case_status = 'in_progress'
+        order by c.created_at";
 
-        if (($casosComAlertStatusDiferentes->total == 0) && ($casosEstruturaDeCasoIncompleta->total == 0) && ($casosCriancasSemInformacaodeStep->total == 0)) {
+        $alertasSemNome = $this->queryObject($sqlAlertasSemNome);
+
+        if (($casosEstruturaDeCasoIncompleta->total == 0) && ($casosCriancasSemInformacaodeStep->total == 0) && ($alertasSemNome->total == 0)) {
             $this->comment('Banco de dados consistente!');
             return;
         }
@@ -88,21 +88,6 @@ class CheckInconsistenciesCases extends Command
             if (strstr($str, '"'))
                 $str = '"' . str_replace('"', '""', $str) . '"';
         }
-
-
-        if ($casosComAlertStatusDiferentes->total > 0):
-            $file = fopen(env('INCONSISTENCIES_FOLDER') . "/alert_status_diferentes_" . date('d_m_Y') . ".csv", 'w');
-            $flag = false;
-            foreach ($casosComAlertStatusDiferentes->values as $child) {
-                if (!$flag) {
-                    fputcsv($file, array_keys($child), ',', '"');
-                    $flag = true;
-                }
-                array_walk($child, __NAMESPACE__ . '\cleanData');
-                fputcsv($file, array_values($child), ',', '"');
-            }
-            fclose($file);
-        endif;
 
         if ($casosEstruturaDeCasoIncompleta->total > 0):
             $file = fopen(env('INCONSISTENCIES_FOLDER') . "/estrutura_tabelas_incompleta_" . date('d_m_Y') . ".csv", 'w');
@@ -133,6 +118,76 @@ class CheckInconsistenciesCases extends Command
             fclose($file);
         endif;
 
+        if ($alertasSemNome->total > 0):
+            $file = fopen(env('INCONSISTENCIES_FOLDER') . "/alertas_sem_nome" . date('d_m_Y') . ".csv", 'w');
+            $flag = false;
+            foreach ($alertasSemNome->values as $child) {
+                if (!$flag) {
+                    fputcsv($file, array_keys($child), ',', '"');
+                    $flag = true;
+                }
+                array_walk($child, __NAMESPACE__ . '\cleanData');
+                fputcsv($file, array_values($child), ',', '"');
+            }
+            fclose($file);
+        endif;
+
+        //remocoes
+        $resposta = $this->ask("Essa ação removerá $casosEstruturaDeCasoIncompleta->total crianças com estrutura de tabelas incompleta, $casosCriancasSemInformacaodeStep->total crianças sem etapa em children_case e $alertasSemNome->total alertas sem o nome e informaçõe essencias da criança. \n Deseja continuar? sim ou não.");
+
+        if ($resposta == 'sim') {
+
+            $totalCasosInconsistentes = 0;
+            if ($casosEstruturaDeCasoIncompleta->total > 0):
+                foreach ($casosEstruturaDeCasoIncompleta->values as $child) {
+                    $id = $child['id'];
+                    $result = $this->excluirCasos($id);
+                    if ($result) {
+                        $this->comment('Criança inconsistente id: ' . $id . ' excluído!');
+                        Log::info('Caso inconsistente exluída id: ' . $id);
+                    } else {
+                        $this->comment('Criança inconsistente id: ' . $id . ' problema ao excluír!');
+                        Log::info('Caso inconsistente problema ao exluir id: ' . $id);
+                    }
+                    $totalCasosInconsistentes++;
+                }
+            endif;
+
+            $totalCasosInconsistentes = 0;
+            if ($casosCriancasSemInformacaodeStep->total > 0):
+                foreach ($casosCriancasSemInformacaodeStep->values as $child) {
+                    $id = $child['id'];
+                    $result = $this->excluirCasos($id);
+                    if ($result) {
+                        $this->comment('Criança inconsistente id: ' . $id . ' excluído!');
+                        Log::info('Caso inconsistente exluída id: ' . $id);
+                    } else {
+                        $this->comment('Criança inconsistente id: ' . $id . ' problema ao excluír!');
+                        Log::info('Caso inconsistente problema ao exluir id: ' . $id);
+                    }
+                    $totalCasosInconsistentes++;
+                }
+            endif;
+
+            $totalCasosInconsistentes = 0;
+            if ($alertasSemNome->total > 0):
+                foreach ($alertasSemNome->values as $child) {
+                    $id = $child['id'];
+                    $result = $this->excluirCasos($id);
+                    if ($result) {
+                        $this->comment('Criança inconsistente id: ' . $id . ' excluído!');
+                        Log::info('Caso inconsistente exluída id: ' . $id);
+                    } else {
+                        $this->comment('Criança inconsistente id: ' . $id . ' problema ao excluír!');
+                        Log::info('Caso inconsistente problema ao exluir id: ' . $id);
+                    }
+                    $totalCasosInconsistentes++;
+                }
+            endif;
+
+        } else {
+            $this->comment('Ação Cancelada! ');
+        }
 
     }
 
@@ -150,5 +205,23 @@ class CheckInconsistenciesCases extends Command
         $obj->total = $totalCriancaInconsistentes;
 
         return $obj;
+    }
+
+    private function excluirCasos($id)
+    {
+        try {
+            DB::table('children')->where('id', $id)->delete();
+            DB::table('children_cases')->where('child_id', $id)->delete();
+            DB::table('case_steps_alerta')->where('child_id', $id)->delete();
+            DB::table('case_steps_analise_tecnica')->where('child_id', $id)->delete();
+            DB::table('case_steps_gestao_do_caso')->where('child_id', $id)->delete();
+            DB::table('case_steps_observacao')->where('child_id', $id)->delete();
+            DB::table('case_steps_pesquisa')->where('child_id', $id)->delete();
+            DB::table('case_steps_rematricula')->where('child_id', $id)->delete();
+            return true;
+
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
