461c2d9d9f1aadf0e0f5a121ecd599136c679fbc
Manoel Filho <43251355+manoelsouzaunicef@users.noreply.github.com>
2023-10-19 12:26:07 -0300

    Unicef01/busca ativa#1475 (#253)
    * Remocao de classe de importcao nao utilizada
    * Corrigindo processo de importação do Educacenso e Arquivo Customizado

--- app/Importers/XLSFileChildrenImporter.php
+++ app/Importers/XLSFileChildrenImporter.php
@@ -6,14 +6,10 @@ use BuscaAtivaEscolar\CaseSteps\Pesquisa;
 use BuscaAtivaEscolar\Child;
 use BuscaAtivaEscolar\Comment;
 use BuscaAtivaEscolar\Data\AlertCause;
-use BuscaAtivaEscolar\Importers\TypeImporters\ChunkEducacensoReadFilter;
 use BuscaAtivaEscolar\Importers\TypeImporters\ChunkMunicipioReadFilter;
 use BuscaAtivaEscolar\ImportJob;
 use BuscaAtivaEscolar\User;
 use Carbon\Carbon;
-use Config;
-use Excel;
-use Log;
 use Exception;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
@@ -63,12 +59,9 @@ class XLSFileChildrenImporter implements Importer
 
         $this->agent = User::find(User::ID_IMPORT_XLS_BOT);
 
-        if(!$this->agent) {
+        if (!$this->agent) {
             throw new Exception("Failed to find Educacenso bot user!");
         }
-
-        Log::info("[xls_import] Tenant {$this->tenant->name}, file {$this->file}");
-        Log::info("[xls_import] Loading spreadsheet data into memory ...");
 
         /** Cria o reader do PhpSpreadsheet */
         $reader = IOFactory::createReader('Xlsx');
@@ -85,143 +78,124 @@ class XLSFileChildrenImporter implements Importer
         for ($startRow = 0; $startRow <= 65536; $startRow += $chunkSize) {
 
             $chunkFilter->setRows($startRow, $chunkSize);
-            $maxRow = ($startRow + $chunkSize)-1;
+            $maxRow = ($startRow + $chunkSize) - 1;
             $spreadsheet = $reader->load($this->file);
-            $records = $spreadsheet->getActiveSheet()->rangeToArray('A'.$startRow.':K'.$maxRow);
+            $records = $spreadsheet->getActiveSheet()->rangeToArray('A' . $startRow . ':K' . $maxRow);
 
             //validacao do cabecalho
-            if($startRow == 0 AND $this->isHeaderPatterFileMunicipio($records[1]) == false){
+            if ($startRow == 0 and $this->isHeaderPatterFileMunicipio($records[1]) == false) {
                 throw new \Exception("Cabeçalho padrão da importação não localizado");
             }
 
             //Validacao de cada bloco do chunk por linha
             foreach ($records as $key => $record) {
-                if( ($startRow == 0 AND $key > 1) OR ($startRow > 0) ) {
-                    if ($record[0] == null) { goto lastLineValidation; }
+                if (($startRow == 0 and $key > 1) or ($startRow > 0)) {
+                    if ($record[0] == null) {
+                        goto lastLineValidation;
+                    }
                     $this->validateRow($record);
                 }
             }
-
         }
 
         lastLineValidation:
-        Log::info("Última linha para validacao localizada");
 
         /** IMPORTACAO DOS DADOS**/
         for ($startRow = 0; $startRow <= 65536; $startRow += $chunkSize) {
 
             $chunkFilter->setRows($startRow, $chunkSize);
-            $maxRow = ($startRow + $chunkSize)-1;
+            $maxRow = ($startRow + $chunkSize) - 1;
             $spreadsheet = $reader->load($this->file);
-            $records = $spreadsheet->getActiveSheet()->rangeToArray('A'.$startRow.':K'.$maxRow);
+            $records = $spreadsheet->getActiveSheet()->rangeToArray('A' . $startRow . ':K' . $maxRow);
 
             foreach ($records as $key => $record) {
-                if( ($startRow == 0 AND $key > 1) OR ($startRow > 0) ) {
+                if (($startRow == 0 and $key > 1) or ($startRow > 0)) {
 
-                    if ($record[0] == null) { goto end; }
-
-                    if( $this->isThereChild($record) == false){
-                        $this->parseChild($record);
+                    if ($record[0] == null) {
+                        goto end;
                     }
 
+                    if ($this->isThereChild($record) == false) {
+                        $this->parseChild($record);
+                    }
                 }
             }
-
         }
 
         end:
-        Log::info("Última linha para importacao localizada");
 
         $job->setTotalRecords($this->total_records);
         $job->setDuplicateds($this->duplicateds);
-
-        Log::info("[educacenso_import] Completed parsing all records");
-        Log::info("[educacenso_import] Job completed!");
-
     }
 
-    public function validateRow($row){
+    public function validateRow($row)
+    {
 
         //Validacoes dos campos obrigatórios
-        if($row[0] == null){
-            Log::info("Nome da criança ou adolescente não informado.");
+        if ($row[0] == null) {
             throw new Exception("Arquivo inválido. Nome da criança não informado");
         }
 
-        if($row[2] == null){
-            Log::info("Nome da mãe ou responsável pela criança ou adolescente não informado.");
+        if ($row[2] == null) {
             throw new Exception("Arquivo inválido. Nome da mãe ou responsável pela criança não informado");
         }
 
-        if($row[6] == null){
-            Log::info("Estado não informado.");
+        if ($row[6] == null) {
             throw new Exception("Arquivo inválido. Estado não informado");
         }
 
-        if($row[7] == null){
-            Log::info("Cidade não informada.");
+        if ($row[7] == null) {
             throw new Exception("Arquivo inválido. Cidade não informada");
         }
 
-        if($row[5] == null){
-            Log::info("Bairro não informado.");
+        if ($row[5] == null) {
             throw new Exception("Arquivo inválido. Bairro não informado");
         }
 
-        if($row[4] == null){
-            Log::info("Endereço não informado.");
+        if ($row[4] == null) {
             throw new Exception("Arquivo inválido. Endereço não informado");
         }
 
         //validacao data de nascimento
         if ($row[1] != null) {
 
-            if( !preg_match("/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/", trim($row[1])) ){
-                Log::info("A data de nascimento não está no padrão XX/XX/XXXX.");
-                throw new Exception("Arquivo inválido. A data de nascimento não está no padrão XX/XX/XXXX - ".$row[0]);
+            if (!preg_match("/[0-9]{2}\/[0-9]{2}\/[0-9]{4}/", trim($row[1]))) {
+                throw new Exception("Arquivo inválido. A data de nascimento não está no padrão XX/XX/XXXX - " . $row[0]);
             }
-            list($dd,$mm,$yyyy) = explode('/', trim($row[1]));
-            if(checkdate($mm,$dd,$yyyy) == false){
-                Log::info("Data de nascimento inválida.");
-                throw new Exception("Arquivo inválido. Data de nascimento inválida - ".$row[0]);
+            list($dd, $mm, $yyyy) = explode('/', trim($row[1]));
+            if (checkdate($mm, $dd, $yyyy) == false) {
+                throw new Exception("Arquivo inválido. Data de nascimento inválida - " . $row[0]);
             }
         }
 
         //validacao telefone
         if ($row[9] != null) {
-            if( strlen ( (string) $row[9] ) < 10 ){
-                Log::info("Número de telefone incompleto.");
-                throw new Exception("Arquivo inválido. Número de telefone incompleto - ".$row[0]);
+            if (strlen((string) $row[9]) < 10) {
+                throw new Exception("Arquivo inválido. Número de telefone incompleto - " . $row[0]);
             }
 
-            if( strlen ( (string) $row[9] ) > 11 ){
-                Log::info("Número de telefone maior que o permitido.");
-                throw new Exception("Arquivo inválido. Número de telefone maior que o permitido - ".$row[0]);
+            if (strlen((string) $row[9]) > 11) {
+                throw new Exception("Arquivo inválido. Número de telefone maior que o permitido - " . $row[0]);
             }
 
-            if( strpos( (string) $row[9], "(" ) OR strpos( (string) $row[9], ")" ) ){
-                Log::info("Número de telefone contém caracteres inválidos.");
-                throw new Exception("Arquivo inválido. Número de telefone contém caracteres inválidos - ".$row[0]);
+            if (strpos((string) $row[9], "(") or strpos((string) $row[9], ")")) {
+                throw new Exception("Arquivo inválido. Número de telefone contém caracteres inválidos - " . $row[0]);
             }
         }
 
         //valida cep
         if ($row[8] != null) {
-            if( strlen ( (string) $row[8] ) < 8 ){
-                Log::info("CEP incompleto.");
-                throw new Exception("Arquivo inválido. CEP incompleto - ".$row[0]);
+            if (strlen((string) $row[8]) < 8) {
+                throw new Exception("Arquivo inválido. CEP incompleto - " . $row[0]);
             }
-            if( strlen ( (string) $row[8] ) > 8 ){
-                Log::info("CEP maior que o permitido.");
-                throw new Exception("Arquivo inválido. CEP maior que o permitido - ".$row[0]);
+            if (strlen((string) $row[8]) > 8) {
+                throw new Exception("Arquivo inválido. CEP maior que o permitido - " . $row[0]);
             }
         }
-
-
     }
 
-    private function parseChild($row){
-        Log::info("[xls_import] Bot Agent User: {$this->agent->id}, {$this->agent->name}");
+    private function parseChild($row)
+    {
 
         $fieldMap = [
             0 => 'name',
@@ -237,8 +211,8 @@ class XLSFileChildrenImporter implements Importer
 
         $data = [];
 
-        foreach($fieldMap as $xlsField => $systemField) {
-            if(!isset($row[$xlsField])) continue;
+        foreach ($fieldMap as $xlsField => $systemField) {
+            if (!isset($row[$xlsField])) continue;
             $data[$systemField] = trim((string) $row[$xlsField]);
         }
 
@@ -250,28 +224,20 @@ class XLSFileChildrenImporter implements Importer
         $data['place_kind'] = null;
         $data['has_been_in_school'] = false;
 
-        Log::info("[xls_import] \t Parsed data: " . print_r($data, true));
-
         $child = Child::spawnFromAlertData($this->tenant, $this->agent->id, $data);
-
-        Log::info("[xls_import] \t Spawned child with ID: {$child->id}");
 
         $pesquisa = Pesquisa::fetchWithinCase($child->current_case_id, Pesquisa::class, 20);
 
-        Log::info("[xls_import] \t Found pesquisa: {$pesquisa->id}");
 
         $pesquisa->setFields($data);
 
-        Log::info("[xls_import] \t Posting comments...");
-
         Comment::post($child, $this->agent, "Caso importado na planilha personalizada do Município");
-
-        Log::info("[xls_import] \t Child spawn complete!");
 
         $this->total_records++;
     }
 
-    private function isThereChild($row){
+    private function isThereChild($row)
+    {
 
         $child = Child::where(
             [
@@ -298,17 +264,16 @@ class XLSFileChildrenImporter implements Importer
             ]
         )->first();
 
-        if($child == null){
+        if ($child == null) {
             return false;
-        }else{
-            Log::info("Child already exists ".$child->name." | ID: ".$child->id);
+        } else {
             array_push($this->duplicateds, $child);
             return true;
         }
-
     }
 
-    public function isHeaderPatterFileMunicipio ($headerArray) {
+    public function isHeaderPatterFileMunicipio($headerArray)
+    {
 
         $headerPatternMunicipio = [
             0 => 'Nome da criança ou adolescente',
@@ -325,7 +290,5 @@ class XLSFileChildrenImporter implements Importer
         ];
 
         return $headerArray == $headerPatternMunicipio;
-
     }
-
 }
